fc4d0cc99419e3fd4dbdadc2c84817ed479600ad
ev83R <ev83RUS@gmail.com>
2019-12-16 11:50:25 +0300

    IP

--- ClientWindow/src/ru/kav/clientwindow/ClientWindow.java
+++ ClientWindow/src/ru/kav/clientwindow/ClientWindow.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
 
-    private static final String IP_ADDR = "10.254.10.145";
+    private static final String IP_ADDR = "192.168.56.1";
     private static final int PORT = 8189;
     private static final int WIDTH = 600;
     private static final int HEIGHT = 400;
